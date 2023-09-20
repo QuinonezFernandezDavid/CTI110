5faba3692b19a2966f21cc76f08f1400a2943eba
QuinonezFernandezDavid <145077083+QuinonezFernandezDavid@users.noreply.github.com>
2023-09-20 15:22:03 -0400

    Add files via upload
    This is my second upload

---
+++ P1HW2_QuinopnezFernandezDavid.py
@@ -0,0 +1,56 @@
+# This will be my first python progrfan
+# 10/1/23
+# CTL-110 P1HW2 - Travel Expense
+# Quinonez fernandez, David
+
+#  Ask user to enter their budget
+# Ask user to enter travel destination
+# Ask user for amount they will spend on gas
+# Ask user for amount they will spend on accommodation
+# Ask user for amount they will spend on food
+# Add expenses
+# Subtract expenses from budget
+# Display results
+
+print('This progran calculate and display travel expenses')
+
+print()
+
+InitialBudget = input('Enter Budget: ')
+
+print()
+
+Location = input('Enter Your travel destination: ')
+
+print()
+
+Fuel = input('How much do you think you will spend on gas? ')
+
+print()
+
+Accomodation = input('Approximately, how much will you need for accomodation/hotel? ')
+
+print()
+
+Foot = input('Last, How mujch do you need for travel foot? ')
+
+print()
+
+print("------------Travel Expenses------------")
+
+print("Location: " + Location)
+
+print("Inicial Budget: " + InitialBudget)
+
+print()
+
+print("Fuel: " + Fuel)
+
+print("Accomodation:" + Accomodation)
+
+print("Foot: " + Foot)
+
+print()
+
+RemainingBalance = int(InitialBudget) - int(Fuel) - int(Accomodation) - int(Foot)
+print("Remaining Balance: " , RemainingBalance)
